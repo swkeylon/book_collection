de1d173b10cf83d1663c5d7b09c6b3b86b9145ce
root <root@localhost>
2023-09-12 22:15:38 +0000

    Modified user_books view and updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :user_books
+  resources :users_path
+  root 'user_books#index'
+
+
   resources :books do
     member do
       get :delete
     end
   end
-  root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
